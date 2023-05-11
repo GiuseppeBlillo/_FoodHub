bd278736d77455ae24e45840433e9278a905ca20
Pietro_Croce <pcroce91@gmail.com>
2023-05-11 19:36:29 +0200

    Aggiunti Primi piatti e rispettate regole di scrittura, codice morto e rinominata classe

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,56 @@ public class Main {
 
         //AGGIUNTA DEGLI INGREDIENTI
         Ingredienti.addIng("olio",0.50);
-        Ingredienti.addIng("pomodoro",0.90);
-        Ingredienti.addIng("ceci",0.70);
+        Ingredienti.addIng("aglio",0.30);
+        Ingredienti.addIng("pomodoro",2.10);
+        Ingredienti.addIng("peperoncino",0.50);
+        Ingredienti.addIng("erbe",0.40);
+        Ingredienti.addIng("porcini",3.20);
+        Ingredienti.addIng("seitan",3.50);
+        Ingredienti.addIng("verdure",2.80);
+        Ingredienti.addIng("soia",3.90);
+        Ingredienti.addIng("legumi",1.70);
+        Ingredienti.addIng("banano",4.50);
         Ingredienti.addIng("farina",0.50);
+        Ingredienti.addIng("tofu",3.40);
+        Ingredienti.addIng("melanzane",2.50);
+        Ingredienti.addIng("olive",1.70);
+        Ingredienti.addIng("agrumi",2.90);
+        Ingredienti.addIng("confettura",2.10);
+        Ingredienti.addIng("cocco",3.00);
+        Ingredienti.addIng("caffe",1.10);
+        Ingredienti.addIng("biscotti",3.10);
+        Ingredienti.addIng("cacao",0.40);
+        Ingredienti.addIng("ciocciolato",4.00);
+        Ingredienti.addIng("pera",1.50);
+        Ingredienti.addIng("risotto",0.90);
+        Ingredienti.addIng("spaghetti",0.70);
+        Ingredienti.addIng("pasta",0.60);
+        Ingredienti.addIng("polpette",3.10);
+        Ingredienti.addIng("arancio",1.90);
 
         //AGGIUNTA DI UNA LISTA PER LE PORTATE
+        List<Primi> primiPiatti = new ArrayList<>();
         //List<Secondi> listaSecondiPiatti = new ArrayList<>();
-
+        Primi spaghetti = new Primi("Spaghetti aglio,olio e peperoncino","Spaghetti saltati su olio extravergine, con base di aglio e peperoncino", false);
+        Primi amalfitana = new Primi("Spaghetti all'Amalfitana","Spaghetti con posa di pomodoro, insaporiti di erbe domestiche", false);
+        Primi orechiette = new Primi("Orecchiette con porcini e seitan","Tipica pasta fresca fatta in casa, con porcini raccolti nei nostri boschi. Il tutto arricchito del nostro miglior seitan", true);
+        Primi arcobaleno = new Primi("Risotto  Arcobaleno","Risotto con verdure colorate, ricoperto da una soffice creme fraiche di soia", true);
         //AGGIUNTA DEI SECONDI
         //Secondi p1 = new Secondi("Carne di manzo","deliziosa carne di manzo con olio e pomodoro", true);
         //Secondi p2 = new Secondi("Carne di pollo","sublime carne di pollo con olio, impanata con farina di ceci");
 
+        primiPiatti.add(spaghetti);
+        primiPiatti.add(amalfitana);
+        primiPiatti.add(orechiette);
+        primiPiatti.add(arcobaleno);
         //listaSecondiPiatti.add(p1);
         //listaSecondiPiatti.add(p2);
 
         //AGGIUNTA DI UN PRINT PER CAPIRE (lol)
         //p1.printFinalPrice();
        // p2.printPrice();
-
+        System.out.println(spaghetti.augPrice());
        // for (Secondi sec: listaSecondiPiatti){
       //      System.out.println(sec.getName() + " " + sec.finalPrice() + "€");
        // }
---
+++ src/Primi.java
@@ -0,0 +1,39 @@
+import java.text.DecimalFormat;
+import java.util.regex.Pattern;
+public class Primi extends Menu implements Methods {
+    public Primi(String name, String description, boolean isGlutenFree) {
+        super(name, description, isGlutenFree);
+    }
+
+    public Primi(String name, String description) {
+        super(name, description);
+    }
+    @Override
+    public double augPrice() {
+        double totPrice = 0.0;
+        String[] ingList = Pattern.compile("\\W+").split(description);
+        for (String ing : ingList) {
+            if (Ingredienti.getIngredients().get(ing) != null) {
+                totPrice += Ingredienti.getIngredients().get(ing);
+            }
+        }return totPrice;
+    }
+    @Override
+    public void printPrice() {
+        DecimalFormat x = new DecimalFormat("#.00");
+        System.out.println(x.format(augPrice()) + "€");
+    }
+    @Override
+    public double finalPrice() {
+        double basePrice = augPrice();
+        double impPrice = menu.SECONDI.setMenuPrice(basePrice);
+        double extraPrice = this.isGlutenFree ? 0.0 : 2.0;
+        DecimalFormat x = new DecimalFormat("#.00");
+        return Double.parseDouble(x.format(impPrice + extraPrice));
+    }
+    @Override
+    public void printFinalPrice() {
+        DecimalFormat x = new DecimalFormat("#.00");
+        System.out.println(finalPrice() + "€");
+    }
+}
